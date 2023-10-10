d0c598ad23e9d357df5023e1a3e2cc8a75d87a23
Alfesh Abdul <alfeshabdul2002@gmail.com>
2023-10-10 16:01:48 +0530

    added component for campaigns

--- src/views/CampaignsPage/CampaignsPage.js
+++ src/views/CampaignsPage/CampaignsPage.js
@@ -1,16 +1,42 @@
 import Footer from "../../components/Footer/Footer"
 import Navbar from "../../components/Navbar/Navbar"
 
-function Campaigns(){
-    return( 
-       <div>
-          <Navbar/>
-         <h1>Campaigns Page</h1>
-          <Footer/>
+import CampaignCard from "../../components/CampaignCard/CampaignCard"
+
+
+const campaignsData = [
+   {
+     id: 1,
+     title: 'Clean Water Initiative',
+     description: 'Provide clean and safe drinking water to communities in need.',
+     goal: '$10,000',
+     raised: '$7,500'
+   },
+   {
+     id: 2,
+     title: 'Education for All',
+     description: 'Support education programs to ensure every child has access to quality education.',
+     goal: '$15,000',
+     raised: '$12,200'
+   },
+
+ ];
+ 
+ function Campaigns() {
+   return (
+     <div className="campaigns-page">
+       <Navbar />
+       <div className="campaigns-container">
+         <h1>Explore Campaigns</h1>
+         <div className="campaigns-list">
+           {campaignsData.map(campaign => (
+             <CampaignCard key={campaign.id} campaign={campaign} />
+           ))}
+         </div>
        </div>
-       
-       
-    )
-   }
-   
-   export default Campaigns
+       <Footer />
+     </div>
+   );
+ }
+ 
+ export default Campaigns
