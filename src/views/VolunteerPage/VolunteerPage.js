54360f38a59922f9c293f1d87cfe6cd705f84507
shivkanya11 <mhetreshivkanya@gmail.com>
2023-10-11 21:54:58 +0530

    form design updates

--- src/views/VolunteerPage/VolunteerPage.js
+++ src/views/VolunteerPage/VolunteerPage.js
@@ -1,14 +1,17 @@
 import Footer from "../../components/Footer/Footer"
 import Navbar from "../../components/Navbar/Navbar"
+import './VolunteerPage.css'
 
 function Volunteer() {
    return (
       <div>
          <Navbar />
          <div className='card'>
-            <Form>
-               <input type={text} placeholder='Enter Name' className=""/>
-            </Form>
+            <form>
+               <input type='text' placeholder='Enter Name' className="user-input"/>
+               <input type='text' placeholder='Enter mail' className="user-input"/>
+               <input type='text' placeholder='Enter mobile no' className="user-input"/>
+            </form>
          </div>
          <Footer />
       </div>
