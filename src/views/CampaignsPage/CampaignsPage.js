6d56775ece6a2f9133138bf6ed66385d6aef8973
Alfesh Abdul <alfeshabdul2002@gmail.com>
2023-10-11 18:40:43 +0530

    added css to components and added data in campaigns

--- src/views/CampaignsPage/CampaignsPage.js
+++ src/views/CampaignsPage/CampaignsPage.js
@@ -1,42 +1,71 @@
+import React from 'react';
+import './Campaigns.css'; 
+import CampaignCard from '../../components/CampaignCard/CampaignCard'; 
+
 import Footer from "../../components/Footer/Footer"
 import Navbar from "../../components/Navbar/Navbar"
 
-import CampaignCard from "../../components/CampaignCard/CampaignCard"
-
-
 const campaignsData = [
-   {
-     id: 1,
-     title: 'Clean Water Initiative',
-     description: 'Provide clean and safe drinking water to communities in need.',
-     goal: 'Rupees:- 10,000',
-     raised: 'Rupees:- 7,500'
-   },
-   {
-     id: 2,
-     title: 'Education for All',
-     description: 'Support education programs to ensure every child has access to quality education.',
-     goal: 'Rupees :- 15,000',
-     raised: 'Rupees :- 12,200'
-   },
+  {
+    id: 1,
+    title: 'Clean Water Initiative',
+    description: 'Cleaning nearby Waterbodies.',
+    goal: 'RUPEES :- 10,000',
+    raised: 'RUPEES :- 7,500'
+  },
+  {
+    id: 2,
+    title: 'Education for All',
+    description: 'Support education programs at Slums .',
+    goal: 'RUPEES :- 15,000',
+    raised: 'RUPEES :- 12,200'
+  },
+  {
+    id: 2,
+    title: 'Animal Care',
+    description: 'Support Street Animals for hunger.',
+    goal: 'RUPEES :- 19,000',
+    raised: 'RUPEES :- 1,800'
+  },
+  {
+    id: 1,
+    title: 'Clean Water Initiative',
+    description: 'Cleaning nearby Waterbodies.',
+    goal: 'RUPEES :- 10,000',
+    raised: 'RUPEES :- 7,500'
+  },
+  {
+    id: 2,
+    title: 'Education for All',
+    description: 'Support education programs at Slums .',
+    goal: 'RUPEES :- 15,000',
+    raised: 'RUPEES :- 12,200'
+  },
+  {
+    id: 2,
+    title: 'Animal Care',
+    description: 'Support Street Animals for hunger.',
+    goal: 'RUPEES :- 19,000',
+    raised: 'RUPEES :- 1,800'
+  },
+  
+];
 
- ];
- 
- function Campaigns() {
-   return (
-     <div className="campaigns-page">
-       <Navbar />
-       <div className="campaigns-container">
-         <h1>Explore Campaigns</h1>
-         <div className="campaigns-list">
-           {campaignsData.map(campaign => (
-             <CampaignCard key={campaign.id} campaign={campaign} />
-           ))}
-         </div>
-       </div>
-       <Footer />
-     </div>
-   );
- }
- 
- export default Campaigns
+function Campaigns() {
+  return (
+    <div className="campaigns-page">
+      <Navbar />
+      <div className="campaigns-container">
+        <h1 className='heading'>Explore Campaigns</h1>
+        <div className="campaigns-list">
+          {campaignsData.map(campaign => (
+            <CampaignCard key={campaign.id} campaign={campaign} />
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Campaigns;
