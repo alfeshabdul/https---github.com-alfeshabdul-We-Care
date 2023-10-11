d431a83df58b56f67c22e922c4582cc9331c984a
Alfesh Abdul <alfeshabdul2002@gmail.com>
2023-10-11 18:43:59 +0530

    added more jsx code

--- src/views/CampaignsPage/CampaignsPage.js
+++ src/views/CampaignsPage/CampaignsPage.js
@@ -26,29 +26,7 @@ const campaignsData = [
     description: 'Support Street Animals for hunger.',
     goal: 'RUPEES :- 19,000',
     raised: 'RUPEES :- 1,800'
-  },
-  {
-    id: 1,
-    title: 'Clean Water Initiative',
-    description: 'Cleaning nearby Waterbodies.',
-    goal: 'RUPEES :- 10,000',
-    raised: 'RUPEES :- 7,500'
-  },
-  {
-    id: 2,
-    title: 'Education for All',
-    description: 'Support education programs at Slums .',
-    goal: 'RUPEES :- 15,000',
-    raised: 'RUPEES :- 12,200'
-  },
-  {
-    id: 2,
-    title: 'Animal Care',
-    description: 'Support Street Animals for hunger.',
-    goal: 'RUPEES :- 19,000',
-    raised: 'RUPEES :- 1,800'
-  },
-  
+  }, 
 ];
 
 function Campaigns() {
