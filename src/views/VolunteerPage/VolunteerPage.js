d58c1049eaefa32c12186cbae1963ba7a2746462
shivkanya11 <mhetreshivkanya@gmail.com>
2023-10-12 20:04:34 +0530

    HeaderSection inserted

--- src/views/VolunteerPage/VolunteerPage.js
+++ src/views/VolunteerPage/VolunteerPage.js
@@ -1,16 +1,28 @@
 import Footer from "../../components/Footer/Footer"
 import Navbar from "../../components/Navbar/Navbar"
+import v1 from './img/v1.jpg'
+import './VolunteerPage.css'
 
-function Volunteer(){
-    return(
-       <div>
-          <Navbar/>
-         <h1>Volunteer Page</h1>
-          <Footer/>
-       </div>
-       
-       
-    )
-   }
-   
-   export default Volunteer
+function Volunteer() {
+   return (
+      <div>
+         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"></link>
+         <Navbar />
+
+         <div className="header-section">
+            <div className="left-section">
+               hi
+            </div>
+            <div className="right-section">
+               <img src={v1} className='brand-img'/>
+            </div>
+         </div>
+
+         <Footer />
+      </div>
+
+
+   )
+}
+
+export default Volunteer
