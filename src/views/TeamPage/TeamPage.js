c1885ad4bc71b18ce7b1dc51ba217b5c4624d4f4
Alfesh Abdul <alfeshabdul2002@gmail.com>
2023-10-15 20:07:23 +0530

    added team page

--- src/views/TeamPage/TeamPage.js
+++ src/views/TeamPage/TeamPage.js
@@ -1,14 +1,186 @@
-import Footer from "../../components/Footer/Footer"
-import Navbar from "../../components/Navbar/Navbar"
+import React from "react";
+import './TeamPage.css'
 
-function Team(){
-    return(
-       <div>
-          <Navbar/>
-         <h1>Team Page</h1>
-          <Footer/>
-       </div>
-       )
-   }
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+
+
+
+
+function Team () {
+        <Navbar/>
+let message='There are many variations of passages of Loren Ipsum available but the \n majority have suffered alteration in some injected humour.';
+    return( 
+        
+        <div section className="section-white">
+      <div className="container">
+        <div className="row">
+<div className="col-md-12 text-center">
+  <h2 className="section-title">
+    The Team Behind Pacifico
+  </h2>
+  <p className="secontion-subtitle">{message}
+  </p>
+</div>
+<div className="col-sm-6 col-md-4">
+    <div className="team-item">
+        <img src="https://tse4.mm.bing.net/th?id=OIP.I_06S6MYmA9xFtpa8A2lmQHaEo&pid=Api&P=0&h=180" className="team-img "alt="pic"></img>
+        <h3> vedprakash</h3>
+        <div className="team-info">
+        <p>head of seo</p>
+        <p>“Since you get more joy out of giving joy to others, you should put a  good deal of thought into the happiness that you are able to give.”</p>
+     <ul className="team-icon">
+
+        <a href="https://r.search.yahoo.com/_ylt=AwrKCXt.EihlYjEQtae7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153791/RO=10/RU=https%3a%2f%2ftwitter.com%2f%3flang%3den-in/RK=2/RS=YTOSqSaheLX_IaM05nXHyhZu6DQ-" className="twitter">
+        <img src="https://cdn-icons-png.flaticon.com/128/3670/3670151.png" className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrPrpqZEyhld7QPlTC7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697154073/RO=10/RU=https%3a%2f%2fwww.instagram.com%2f%3fhl%3den/RK=2/RS=nDiQxbYcsivfErI5xB3dreutVX4-" className="pinterest">
+ <img src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrKAu6sEShlWQ0PLl27HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153580/RO=10/RU=https%3a%2f%2fwww.facebook.com%2f/RK=2/RS=SDBmmDXH.xHrSG6t45zo617GcM8-" className="facebook">
+<img src="https://cdn-icons-png.flaticon.com/128/5968/5968764.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=Awrx.1LdEihlXyAQQ7i7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153886/RO=10/RU=https%3a%2f%2fwww.roadtocode.org%2f/RK=2/RS=YBGqu6wRyL9Uyb7Hc9YLQSVMYpc-" className="dribbble">
+<img src="https://www.roadtocode.org/static/media/logo.8dd0bf20731ffeceb4ee.png"className="symbol"></img>
+        </a>&nbsp;
+     </ul>
+    </div>
+</div>
+</div>
+<div className="col-sm-6 col-md-4">
+    <div className="team-item">
+        <img src="https://tse4.mm.bing.net/th?id=OIP.I_06S6MYmA9xFtpa8A2lmQHaEo&pid=Api&P=0&h=180" className="team-img "alt="pic"></img>
+        <h3> vedprakash</h3>
+        <div className="team-info">
+        <p>head of seo</p>
+        <p>“Since you get more joy out of giving joy to others, you should put a  good deal of thought into the happiness that you are able to give.”</p>
+     <ul className="team-icon">
+
+        <a href="https://r.search.yahoo.com/_ylt=AwrKCXt.EihlYjEQtae7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153791/RO=10/RU=https%3a%2f%2ftwitter.com%2f%3flang%3den-in/RK=2/RS=YTOSqSaheLX_IaM05nXHyhZu6DQ-" className="twitter">
+        <img src="https://cdn-icons-png.flaticon.com/128/3670/3670151.png" className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrPrpqZEyhld7QPlTC7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697154073/RO=10/RU=https%3a%2f%2fwww.instagram.com%2f%3fhl%3den/RK=2/RS=nDiQxbYcsivfErI5xB3dreutVX4-" className="pinterest">
+ <img src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrKAu6sEShlWQ0PLl27HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153580/RO=10/RU=https%3a%2f%2fwww.facebook.com%2f/RK=2/RS=SDBmmDXH.xHrSG6t45zo617GcM8-" className="facebook">
+<img src="https://cdn-icons-png.flaticon.com/128/5968/5968764.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=Awrx.1LdEihlXyAQQ7i7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153886/RO=10/RU=https%3a%2f%2fwww.roadtocode.org%2f/RK=2/RS=YBGqu6wRyL9Uyb7Hc9YLQSVMYpc-" className="dribbble">
+<img src="https://www.roadtocode.org/static/media/logo.8dd0bf20731ffeceb4ee.png"className="symbol"></img>
+        </a>&nbsp;
+     </ul>
+    </div>
+</div>
+</div>
+<div className="col-sm-6 col-md-4">
+    <div className="team-item">
+        <img src="https://tse4.mm.bing.net/th?id=OIP.I_06S6MYmA9xFtpa8A2lmQHaEo&pid=Api&P=0&h=180" className="team-img "alt="pic"></img>
+        <h3> vedprakash</h3>
+        <div className="team-info">
+        <p>head of seo</p>
+        <p>“Since you get more joy out of giving joy to others, you should put a  good deal of thought into the happiness that you are able to give.”</p>
+     <ul className="team-icon">
+
+        <a href="https://r.search.yahoo.com/_ylt=AwrKCXt.EihlYjEQtae7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153791/RO=10/RU=https%3a%2f%2ftwitter.com%2f%3flang%3den-in/RK=2/RS=YTOSqSaheLX_IaM05nXHyhZu6DQ-" className="twitter">
+        <img src="https://cdn-icons-png.flaticon.com/128/3670/3670151.png" className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrPrpqZEyhld7QPlTC7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697154073/RO=10/RU=https%3a%2f%2fwww.instagram.com%2f%3fhl%3den/RK=2/RS=nDiQxbYcsivfErI5xB3dreutVX4-" className="pinterest">
+ <img src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrKAu6sEShlWQ0PLl27HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153580/RO=10/RU=https%3a%2f%2fwww.facebook.com%2f/RK=2/RS=SDBmmDXH.xHrSG6t45zo617GcM8-" className="facebook">
+<img src="https://cdn-icons-png.flaticon.com/128/5968/5968764.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=Awrx.1LdEihlXyAQQ7i7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153886/RO=10/RU=https%3a%2f%2fwww.roadtocode.org%2f/RK=2/RS=YBGqu6wRyL9Uyb7Hc9YLQSVMYpc-" className="dribbble">
+<img src="https://www.roadtocode.org/static/media/logo.8dd0bf20731ffeceb4ee.png"className="symbol"></img>
+        </a>&nbsp;
+     </ul>
+    </div>
+</div>
+</div>
+<div className="col-sm-6 col-md-4">
+    <div className="team-item">
+        <img src="https://tse4.mm.bing.net/th?id=OIP.I_06S6MYmA9xFtpa8A2lmQHaEo&pid=Api&P=0&h=180" className="team-img "alt="pic"></img>
+        <h3> vedprakash</h3>
+        <div className="team-info">
+        <p>head of seo</p>
+        <p>“Since you get more joy out of giving joy to others, you should put a  good deal of thought into the happines that you are able to give.”</p>
+     <ul className="team-icon">
+
+        <a href="https://r.search.yahoo.com/_ylt=AwrKCXt.EihlYjEQtae7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153791/RO=10/RU=https%3a%2f%2ftwitter.com%2f%3flang%3den-in/RK=2/RS=YTOSqSaheLX_IaM05nXHyhZu6DQ-" className="twitter">
+        <img src="https://cdn-icons-png.flaticon.com/128/3670/3670151.png" className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrPrpqZEyhld7QPlTC7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697154073/RO=10/RU=https%3a%2f%2fwww.instagram.com%2f%3fhl%3den/RK=2/RS=nDiQxbYcsivfErI5xB3dreutVX4-" className="pinterest">
+ <img src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrKAu6sEShlWQ0PLl27HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153580/RO=10/RU=https%3a%2f%2fwww.facebook.com%2f/RK=2/RS=SDBmmDXH.xHrSG6t45zo617GcM8-" className="facebook">
+<img src="https://cdn-icons-png.flaticon.com/128/5968/5968764.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=Awrx.1LdEihlXyAQQ7i7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153886/RO=10/RU=https%3a%2f%2fwww.roadtocode.org%2f/RK=2/RS=YBGqu6wRyL9Uyb7Hc9YLQSVMYpc-" className="dribbble">
+<img src="https://www.roadtocode.org/static/media/logo.8dd0bf20731ffeceb4ee.png"className="symbol"></img>
+        </a>&nbsp;
+     </ul>
+    </div>
+</div>
+</div>
+<div className="col-sm-6 col-md-4">
+    <div className="team-item">
+        <img src="https://tse4.mm.bing.net/th?id=OIP.I_06S6MYmA9xFtpa8A2lmQHaEo&pid=Api&P=0&h=180" className="team-img "alt="pic"></img>
+        <h3> vedprakash</h3>
+        <div className="team-info">
+        <p>head of seo</p>
+        <p>“Since you get more joy out of giving joy to other, you should put a  good deal of thought into the happiness that you are able to give.”</p>
+     <ul className="team-icon">
+
+        <a href="https://r.search.yahoo.com/_ylt=AwrKCXt.EihlYjEQtae7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153791/RO=10/RU=https%3a%2f%2ftwitter.com%2f%3flang%3den-in/RK=2/RS=YTOSqSaheLX_IaM05nXHyhZu6DQ-" className="twitter">
+        <img src="https://cdn-icons-png.flaticon.com/128/3670/3670151.png" className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrPrpqZEyhld7QPlTC7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697154073/RO=10/RU=https%3a%2f%2fwww.instagram.com%2f%3fhl%3den/RK=2/RS=nDiQxbYcsivfErI5xB3dreutVX4-" className="pinterest">
+ <img src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrKAu6sEShlWQ0PLl27HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153580/RO=10/RU=https%3a%2f%2fwww.facebook.com%2f/RK=2/RS=SDBmmDXH.xHrSG6t45zo617GcM8-" className="facebook">
+<img src="https://cdn-icons-png.flaticon.com/128/5968/5968764.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=Awrx.1LdEihlXyAQQ7i7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153886/RO=10/RU=https%3a%2f%2fwww.roadtocode.org%2f/RK=2/RS=YBGqu6wRyL9Uyb7Hc9YLQSVMYpc-" className="dribbble">
+<img src="https://www.roadtocode.org/static/media/logo.8dd0bf20731ffeceb4ee.png"className="symbol"></img>
+        </a>&nbsp;
+     </ul>
+    </div>
+</div>
+</div>
+<div className="col-sm-6 col-md-4">
+    <div className="team-item">
+        <img src="https://tse4.mm.bing.net/th?id=OIP.I_06S6MYmA9xFtpa8A2lmQHaEo&pid=Api&P=0&h=180" className="team-img "alt="pic"></img>
+        <h3> vedprakash</h3>
+        <div className="team-info">
+        <p>head of seo</p>
+        <p>“Since you get more joy out of giving joy to other, you should put a  good deal of thought into the happines that you are able to give.”</p>
+     <ul className="team-icon">
+
+        <a href="https://r.search.yahoo.com/_ylt=AwrKCXt.EihlYjEQtae7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153791/RO=10/RU=https%3a%2f%2ftwitter.com%2f%3flang%3den-in/RK=2/RS=YTOSqSaheLX_IaM05nXHyhZu6DQ-" className="twitter">
+        <img src="https://cdn-icons-png.flaticon.com/128/3670/3670151.png" className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrPrpqZEyhld7QPlTC7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697154073/RO=10/RU=https%3a%2f%2fwww.instagram.com%2f%3fhl%3den/RK=2/RS=nDiQxbYcsivfErI5xB3dreutVX4-" className="pinterest">
+ <img src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=AwrKAu6sEShlWQ0PLl27HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153580/RO=10/RU=https%3a%2f%2fwww.facebook.com%2f/RK=2/RS=SDBmmDXH.xHrSG6t45zo617GcM8-" className="facebook">
+<img src="https://cdn-icons-png.flaticon.com/128/5968/5968764.png"className="symbol"></img>
+        </a>&nbsp;
+        <a href="https://r.search.yahoo.com/_ylt=Awrx.1LdEihlXyAQQ7i7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1697153886/RO=10/RU=https%3a%2f%2fwww.roadtocode.org%2f/RK=2/RS=YBGqu6wRyL9Uyb7Hc9YLQSVMYpc-" className="dribbble">
+<img src="https://www.roadtocode.org/static/media/logo.8dd0bf20731ffeceb4ee.png"className="symbol"></img>
+        </a>&nbsp;
+     </ul>
+    </div>
    
-   export default Team
+</div>
+</div>
+<Footer/>
+ </div>
+</div>
+</div>
+
+
+  )
+}
+
+    
+export default Team;
